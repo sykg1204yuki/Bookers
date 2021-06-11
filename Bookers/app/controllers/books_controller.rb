38d5cad45c944b731bdb894c52ba98d66f67e474
sykg1204yuki <y.yuki040507@gmail.com>
2021-06-11 05:20:26 +0000

    Bookers作成【フラッシュメッセージまで】

---
+++ Bookers/app/controllers/books_controller.rb
@@ -0,0 +1,53 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+     @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    if book.save
+      # 新規投稿成功時
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(book.id)
+    else
+      render :index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      #編集成功時
+      flash[:notice] = 'Book was successfully updated'
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    if @book.destroy
+      flash[:notice] = 'Book was successfully destroyed'
+      redirect_to books_path
+    end
+  end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
+end
