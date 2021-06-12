52d1220e84da9a8b4f3f27d1b40dd98d4e81dcf0
sykg1204yuki <y.yuki040507@gmail.com>
2021-06-12 00:20:48 +0000

    Bookers完成

--- Bookers/app/controllers/books_controller.rb
+++ Bookers/app/controllers/books_controller.rb
@@ -1,16 +1,16 @@
 class BooksController < ApplicationController
   def index
     @books = Book.all
-     @book = Book.new
+    @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
-      # 新規投稿成功時
+    @book = Book.new(book_params)
+    if @book.save #新規投稿成功時
       flash[:notice] = 'Book was successfully created.'
-      redirect_to book_path(book.id)
+      redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render :index
     end
   end
@@ -30,13 +30,13 @@ class BooksController < ApplicationController
       flash[:notice] = 'Book was successfully updated'
       redirect_to book_path(@book.id)
     else
-      render :index
+      render :edit
     end
   end
 
   def destroy
-    @book = Book.find(params[:id])
-    if @book.destroy
+    book = Book.find(params[:id])
+    if book.destroy
       flash[:notice] = 'Book was successfully destroyed'
       redirect_to books_path
     end
